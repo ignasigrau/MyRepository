2e5b3603a79ce574bdd19797fb2c567a8f8f131e
Mona Lisa <ignasi@Ignasis-MacBook-Air.local>
2023-09-15 21:46:30 +0200

    IsPrine

--- Test.py
+++ Test.py
@@ -1,20 +1,23 @@
 def prime_list(last_num):
     list_prime_num = [2]
     for i in range(2,last_num):
-        print(i)
         div = False
         for j in list_prime_num:
             if (i%j)==0:
                 div=True
                 break
         if not div: list_prime_num.append(i)
-
     return list_prime_num
+
+def is_prime(num):
+    if num in prime_list(num+1):
+        return True
+    else:
+        return False
+
 
 if __name__ == '__main__':
     print("Hello world")
-    print(prime_list(1000))
-
 
 
 
